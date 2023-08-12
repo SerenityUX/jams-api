2273612cdbcd33704af6077b99db30933bf4d127
Arpan Pandey <arpanpandey.aps@gmail.com>
2023-08-12 18:49:32 +0530

    Added the API gateway

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # JAMS API
 A basic API for JAMS. This is a work in progress.
+
+## Supported routes
+
+| Route | Method | Supported? |
+| --- | --- |  --- |
+| `/openai/models` | GET | :check_mark: |
+| `/openai/models/:model` | GET | :check_mark: |
+| `/openai/chat/completions` | POST | :check_mark: |
+| `images/generations` | POST | :check_mark: |
+| `/embeddings` | POST | :check_mark: |
+
+Rest of the routes are not supported yet.
+
+## Usage
+For OpenAI API, just replace the `https://api.openai.com/v1/` with `https://jamsapi.herokuapp.com/openai/` and you're good to go.
--- jamsapi/main.py
+++ jamsapi/main.py
@@ -1,7 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+import jamsapi.router.openai as openai_requests
 
-app = FastAPI()
+app = FastAPI(
+    title="JAMS API",
+    description="An API for Jams services and an API gateway for other needed services.",
+    version="0.0.1",
+    contact={
+        "name": "Arpan Pandey",
+        "email": "arpan@hackclub.com"
+    }
+
+)
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Hello World, from JAMS API! Head over to /docs or /redoc to see the API documentation."}
+
+
+@app.get("/openai/models")
+def models(response: Response):
+    """
+        Get the list of models available on OpenAI API
+    """
+    response.status_code = openai_requests.models()[1]
+    return openai_requests.models()[0]
+
+@app.get("/openai/models/{model_name}")
+def model(model_name: str, response: Response):
+    """
+        Get the details of a model available on OpenAI API
+    """
+    response.status_code = openai_requests.model(model_name)[1]
+    return openai_requests.model(model_name)[0]
+
+@app.post("/openai/chat/completions")
+def post_chat_completions(data: dict, response: Response):
+    """
+        Post a chat to OpenAI API
+    """
+    response.status_code = openai_requests.post_chat_completions(data)[1]
+    return openai_requests.post_chat_completions(data)[0]
+
+@app.post("/openai/images/generations")
+def create_image(data: dict, response: Response):
+    """
+        Create an image on OpenAI API
+    """
+    response.status_code = openai_requests.create_image(data)[1]
+    return openai_requests.create_image(data)[0]
+
+
+@app.post("/openai/embeddings")
+def embeddings(data: dict, response: Response):
+    """
+        Get the embeddings of a text on OpenAI API
+    """
+    response.status_code = openai_requests.embeddings(data)[1]
+    return openai_requests.embeddings(data)[0]
