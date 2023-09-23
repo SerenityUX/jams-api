d5c5be6b81117ea9cf58f642ee0f5af1f6bd8bba
Arpan Pandey <arpanpandey.aps@gmail.com>
2023-09-23 16:39:30 +0530

    FIxes

--- jamsapi/openai/tokens.py
+++ jamsapi/openai/tokens.py
@@ -64,7 +64,7 @@ def get_last_revoked_token_by_slack_id(slack_id):
     #! This is a very inefficient way to do this, but I don't know how to do it better with pyairtable at the moment :(
     tokens = OpenAIToken.all()
     # Sort by creation date (oldest first)
-    tokens = sorted(tokens, key=lambda x: x.to_record().get("createdTime"))
+    tokens = sorted(tokens, key=lambda x: x.to_record().get("createdTime"), reverse=True)
     for t in tokens:
         if t.slack_id == slack_id and t.status == "Revoked":
             return t
